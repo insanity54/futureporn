16664a4e14c4906e920ac6e7b99dbec739c3be9d
Chris Grimmett <chris@grimtech.net>
2021-08-31 18:00:25 -0700

    add vultr dynamic inventory script

---
+++ inventory.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+require('dotenv').config();
+const argv = require('minimist')(process.argv.slice(2));
+const fetch = require('node-fetch');
+const R = require('ramda');
+const RA = require('ramda-adjunct');
+
+const vultrApiKey = process.env.VULTR_API_KEY;
+const vultrApiEndpoint = 'https://api.vultr.com';
+const vultrApiFunction = '/v1/server/list';
+if (typeof vultrApiKey === 'undefined') throw new Error('VULTR_API_KEY must be defined in env, but it was undefined');
+
+const { list, host } = argv;
+
+const url = `${vultrApiEndpoint}${vultrApiFunction}?api_key=${vultrApiKey}`;
+
+
+async function getMachines (tag) {
+    const result = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const machines = await result.json();
+
+    const machineHasTag = R.compose(
+        R.includes(tag),
+        R.prop('tag')
+    )
+    const matchingMachines = R.compose(
+        R.filter(machineHasTag)
+    )(machines)
+
+
+    const getMachineIp = R.prop('main_ip')
+    const getMachineLabel = R.prop('label')
+
+    const getMachineData = (machine) => {
+        return {
+            inventory_hostname: getMachineIp(machine),
+        }
+    }
+
+
+    const hostData = R.map(getMachineData, matchingMachines);
+    const machinesNames = R.compose(
+        R.keys
+    )(matchingMachines);
+
+
+    const inventory = {
+        '_meta': {
+            'hostvars': hostData
+        },
+        'all': {
+            'children': [
+                'ungrouped'
+            ]
+        },
+        'ungrouped': machinesNames
+    }
+
+    // https://docs.ansible.com/ansible/latest/dev_guide/developing_inventory.html#developing-inventory-scripts
+    console.log(JSON.stringify(inventory, 0, 2))
+
+
+    return inventory;
+
+}
+
+
+getMachines('immls');
