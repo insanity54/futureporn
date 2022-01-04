2d697069e00608a3fb65506a63c9bd3a7d47bb4c
Chris Grimmett <chris@grimtech.net>
2022-01-03 23:30:41 -0800

    progress

--- utils/tweetProcess.js
+++ utils/tweetProcess.js
@@ -1,6 +1,7 @@
+const VOD = require('./VOD.js');
+const debug = require('debug')('futureporn');
 
-
-export function deriveTitle (text) {
+const deriveTitle = (text) => {
 	// greetz https://www.urlregex.com/
 	const urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
 	let title = text
@@ -11,7 +12,7 @@ export function deriveTitle (text) {
 }
 
 
-export function getFullTweetText (tweet) {
+const getFullTweetText = (tweet) => {
 	let truncated = tweet.truncated;
 	if (truncated) return tweet.extended_tweet.full_text;
 	else return tweet.text;
@@ -21,25 +22,32 @@ export function getFullTweetText (tweet) {
 /**
  * Does stuff with filtered tweets. (side-effects)
  */
-export function processTweet (tweet) {
-	// console.log(`processTweet() is as follows \n${JSON.stringify(tweet, 0, 2)}`);
+const processTweet = async (tweet) => {
+	debug(`processTweet() is as follows \n${JSON.stringify(tweet, 0, 2)}`);
 	const id = (tweet.id || tweet.user.id);
-	console.log('>>> Processing Tweet');
-	console.log(tweet);
+	debug('>>> Processing Tweet');
+	debug(tweet);
 	let tweetId = tweet.id_str;
 	let tweetText = getFullTweetText(tweet);
 	let date = tweet.timestamp_ms;
 	let screenName = tweet.user.screen_name;
 	let announceUrl = `https://twitter.com/${screenName}/status/${tweetId}`;
 	let announceTitle = deriveTitle(tweetText);
-	console.log(`announceTitle: ${announceTitle}`);
-	console.log(`announceUrl: ${announceUrl}`);
-	// @TODO add a handler... Logic that puts the announceTitle & announceUrl into the db
+	debug(`announceTitle: ${announceTitle}`);
+	debug(`announceUrl: ${announceUrl}`);
+	console.log(`[*] MelTweet: ${tweetId} ${date}`)
+	const vod = new VOD({
+		date,
+		announceTitle,
+		announceUrl
+	})
+	vod.saveMarkdown();
+
 }
 
 
-// module.exports = {
-// 	deriveTitle,
-// 	processTweet,
-// 	getFullTweetText
-// }
+module.exports = {
+	deriveTitle,
+	processTweet,
+	getFullTweetText
+}
