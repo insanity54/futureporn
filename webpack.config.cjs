5692a846841f01d9ce7dd4153bf6618dbc609479
Chris Grimmett <chris@grimtech.net>
2022-02-15 12:09:24 -0800

    experiment with jsipfs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,18 +1,53 @@
 const path = require("path");
+const webpack = require('webpack')
 const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const { merge } = require('webpack-merge')
 
-const isDev = process.env.NODE_ENV === "development";
 
+const isDev = (process.env.NODE_ENV === "development");
 const baseFilename = isDev ? "index" : "index.[contenthash]";
 
-module.exports = {
-  mode: isDev ? "development" : "production",
+
+const paths = {
+  build: path.resolve(__dirname, "_site", "assets")
+}
+
+
+const prod = {
+  mode: 'production',
+  devtool: false,
+  output: {
+    path: paths.build,
+    publicPath: '/',
+    filename: '[name].[contenthash].bundle.js'
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  // fix: https://github.com/webpack/webpack-dev-server/issues/2758
+  target: 'browserslist'
+}
+
+
+const dev = {
+  // Set the mode to development or production
+  mode: 'development',
+
+  // Control how source maps are generated
+  devtool: 'inline-source-map'
+}
+
+
+const common = {
   entry: [
     path.resolve(__dirname, "website", "js", "index.js")
   ],
   output: {
-    path: path.resolve(__dirname, "_site", "assets"),
+    path: paths.build,
     filename: `${baseFilename}.js`,
   },
   module: {
@@ -23,12 +58,21 @@ module.exports = {
       },
       {
         test: /\.m?js$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"],
-          },
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    esmodules: true
+                  }
+                }
+              ]
+            ],
+          }
         },
       },
       {
@@ -47,9 +91,23 @@ module.exports = {
       }
     ],
   },
-  devtool: isDev ? "eval" : "source-map",
   plugins: [
     new MiniCssExtractPlugin({ filename: `${baseFilename}.css` }),
     new WebpackManifestPlugin({ publicPath: "/assets/" }),
+    // fixes Module not found: Error: Can't resolve 'stream' in '.../node_modules/nofilter/lib'
+    new NodePolyfillPlugin(),
+    // Note: stream-browserify has assumption about `Buffer` global in its
+    // dependencies causing runtime errors. This is a workaround to provide
+    // global `Buffer` until https://github.com/isaacs/core-util-is/issues/29
+    // is fixed.
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser'
+    }),
   ],
 };
+
+module.exports = () => {
+  const config = isDev ? dev : prod
+  return merge(common, config)
+}
--- website/js/index.js
+++ website/js/index.js
@@ -2,3 +2,46 @@ import '../css/index.scss';
 
 import Plyr from 'plyr';
 new Plyr('#player');
+
+import { create } from 'ipfs-core'
+import Hls from 'hls.js'
+import HlsjsIpfsLoader from 'hlsjs-ipfs-loader'
+
+(async () => {
+  if (window.ipfs === undefined) {
+    const repoPath = 'ipfs-futureporn';
+    const node = await create({
+      repo: repoPath
+    })
+    handleInit(node)
+  } else {
+    window.ipfs.enable().then(handleInit)
+  }
+
+  function handleInit(node) {
+    console.log('handleInit');
+    console.log(node);
+    //https://dweb.link/ipfs/bafybeiflu5ns2auqvqlpxqu2cqprmmn3gjgvxt72akd5kdbhnakyewcyhq?filename=projektmelody-chaturbate-20200606T000211Z-source.mp4
+    const url = "https://dweb.link/ipfs/bafybeig6wi5nbi64bedu6lxkd7ddkgcnagblhds4wvcfux5crgfubv5avi"
+    const filename = "projektmelody-chaturbate-2022-02-13.mp4"
+    const testhash = "bafybeidhrsuwcuibaqoya4ufzgzjz3xiqj3otvkudv62rmnn3qjl6yb7sa";
+    Hls.DefaultConfig.loader = HlsjsIpfsLoader;
+    Hls.DefaultConfig.debug = false;
+    if (Hls.isSupported()) {
+      const video = document.getElementById('player');
+      const hls = new Hls();
+      hls.config.ipfs = node;
+      hls.config.ipfsHash = testhash;
+      hls.loadSource(filename);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        console.log('lets play')
+        video.play();
+      });
+      hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+        console.log('video and hls.js are now bound together !');
+        video.play();
+      });
+    }
+  }
+})()
