a0cd3d4bddd17cfeb5fbfae54fc45f6ab0d16770
Chris Grimmett <chris@grimtech.net>
2022-08-28 22:19:54 -0700

    add logic which removes invalid VOD.tmpFilePath

--- futureporn.mjs
+++ futureporn.mjs
@@ -46,11 +46,13 @@ console.log(`
 
 	console.log(`There are ${vods.length} known VODs`);
 	// process latest vods first
-	vods.reverse()
+	vods.reverse();
+
+	let errorCount = [];
 
 	for (const vod of vods) {
 
-		console.log(`vod:${vod.getDatestamp()} processing begin`);
+		console.log(`  [*] vod:${vod.getDatestamp()} processing begin`);
 
 		const ensuranceFunctions = [
 			// vod.ensureAnnounceUrl,   // idk why I commented this out
@@ -73,14 +75,37 @@ console.log(`
 			try {
 				await f.apply(vod);
 			} catch (e) {
-				console.warn(e);
+
+				errorCount += 1;
+
+				// we want to delete VOD.tmpFilePath if the tmpFilePath is not valid.
+				// we do this because we have not been cleaning up after ourselves on this field
+				// we have a bunch of paths on disk that are irrelevant in production capture/manager/worker environments.
+				// we only want to delete the tmpFilePath if the referenced video is actually absent,
+				// so we check for Permission denied (we see this on local dev laptop when referencing /root/... file paths that are only valid on VPS)
+				// we also check for FileNotFoundErrors 
+				if (e?.stderr) {
+					if (
+						/Permission denied/.test(e.stderr) ||
+						/FileNotFoundError/.test(e.stderr)
+					) {
+						console.log('  !!! VOD.tmpFilePath is not valid so we are deleting it.');
+						vod.tmpFilePath = '';
+						await vod.saveMarkdown();
+					}
+				}
+				console.log('[*********] CAUGHTEM')
+				console.warn(JSON.stringify(e, 0, 2));
+				if (e.type === 'FileNotFoundError') {
+					console.error('EYYYYYYY\nEYYYYYYY\nEYYYYYYY\nEYYYYYYY\nEYYYYYYY\nEYYYYYYY\nEYYYYYYY\nEYYYYYYY\nEYYYYYYY\nEYYYYYYY\n')
+				}
 			}
 		}
 
-		console.log(`vod:${vod.getDatestamp()} complete`)
+		console.log(`  [*] vod:${vod.getDatestamp()} complete`)
 
-
-		// process.exit()
 	}
+
+	console.log(`  [$] Completed with ${errorCount.length} errors.`);
 
 })()
