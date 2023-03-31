8d290cffb3a800fdb0481bb931aed0bb19b3a9cd
Chris Grimmett <chris@grimtech.net>
2023-03-30 22:52:26 -0800

    remove old note

---
+++ packages/builder/_site/assets/index.302e28bf.js
@@ -0,0 +1,87 @@
+import "./modulepreload-polyfill.c7c6310f.js";
+import { l as load, _ as __vitePreload, p as propsById } from "./_slinkity_props_inputPath_.__website_vod-pages.c9a7cf4c.js";
+import "./base.80db5fc5.js";
+/* empty css                 */const props$3 = {};
+for (let propId of ["sFsk1c", "jqy_x0", "wUARDP", "0Ccljk", "_nb9BI"]) {
+  const { name, value } = propsById[propId];
+  props$3[name] = value;
+}
+const target$3 = document.querySelector('slinkity-root[data-id="bN11F_"]');
+Promise.race([
+  load()
+]).then(async function() {
+  const [{ default: Component }, { default: renderer }] = await Promise.all([
+    __vitePreload(() => import("./VideoPlayer.c86cf785.js"), true ? ["assets/VideoPlayer.c86cf785.js","assets/_slinkity_props_inputPath_.__website_vod-pages.c9a7cf4c.js","assets/_slinkity_props_inputPath_.7b587928.css","assets/store.78950e7d.js","assets/common.5d0d2fe0.js"] : void 0),
+    __vitePreload(() => import("./client.5b156202.js"), true ? [] : void 0)
+  ]);
+  renderer({
+    Component,
+    target: target$3,
+    props: props$3,
+    slots: { "default": "" },
+    isClientOnly: false
+  });
+});
+const props$2 = {};
+for (let propId of ["e93N6E", "JbL6bC"]) {
+  const { name, value } = propsById[propId];
+  props$2[name] = value;
+}
+const target$2 = document.querySelector('slinkity-root[data-id="oma1vP"]');
+Promise.race([
+  load()
+]).then(async function() {
+  const [{ default: Component }, { default: renderer }] = await Promise.all([
+    __vitePreload(() => import("./GatewaySelector.e0ec0ddf.js"), true ? ["assets/GatewaySelector.e0ec0ddf.js","assets/store.78950e7d.js"] : void 0),
+    __vitePreload(() => import("./client.5b156202.js"), true ? [] : void 0)
+  ]);
+  renderer({
+    Component,
+    target: target$2,
+    props: props$2,
+    slots: { "default": "" },
+    isClientOnly: false
+  });
+});
+const props$1 = {};
+for (let propId of ["DrNGw6"]) {
+  const { name, value } = propsById[propId];
+  props$1[name] = value;
+}
+const target$1 = document.querySelector('slinkity-root[data-id="9eaHGy"]');
+Promise.race([
+  load()
+]).then(async function() {
+  const [{ default: Component }, { default: renderer }] = await Promise.all([
+    __vitePreload(() => import("./IpfsLink.b7207e56.js"), true ? ["assets/IpfsLink.b7207e56.js","assets/store.78950e7d.js","assets/common.5d0d2fe0.js"] : void 0),
+    __vitePreload(() => import("./client.5b156202.js"), true ? [] : void 0)
+  ]);
+  renderer({
+    Component,
+    target: target$1,
+    props: props$1,
+    slots: { "default": "" },
+    isClientOnly: false
+  });
+});
+const props = {};
+for (let propId of ["SffxQV"]) {
+  const { name, value } = propsById[propId];
+  props[name] = value;
+}
+const target = document.querySelector('slinkity-root[data-id="_VxErX"]');
+Promise.race([
+  load()
+]).then(async function() {
+  const [{ default: Component }, { default: renderer }] = await Promise.all([
+    __vitePreload(() => import("./IpfsLink.b7207e56.js"), true ? ["assets/IpfsLink.b7207e56.js","assets/store.78950e7d.js","assets/common.5d0d2fe0.js"] : void 0),
+    __vitePreload(() => import("./client.5b156202.js"), true ? [] : void 0)
+  ]);
+  renderer({
+    Component,
+    target,
+    props,
+    slots: { "default": "" },
+    isClientOnly: false
+  });
+});
---
+++ packages/builder/_site/assets/index.88b63317.js
@@ -0,0 +1,87 @@
+import "./modulepreload-polyfill.c7c6310f.js";
+import { l as load, _ as __vitePreload, p as propsById } from "./_slinkity_props_inputPath_.__website_vod-pages.c9a7cf4c.js";
+import "./base.80db5fc5.js";
+/* empty css                 */const props$3 = {};
+for (let propId of ["cc1Cp1", "HiJp44", "sPha0w", "oDWtxM", "AalQ3u"]) {
+  const { name, value } = propsById[propId];
+  props$3[name] = value;
+}
+const target$3 = document.querySelector('slinkity-root[data-id="aBR9ZP"]');
+Promise.race([
+  load()
+]).then(async function() {
+  const [{ default: Component }, { default: renderer }] = await Promise.all([
+    __vitePreload(() => import("./VideoPlayer.c86cf785.js"), true ? ["assets/VideoPlayer.c86cf785.js","assets/_slinkity_props_inputPath_.__website_vod-pages.c9a7cf4c.js","assets/_slinkity_props_inputPath_.7b587928.css","assets/store.78950e7d.js","assets/common.5d0d2fe0.js"] : void 0),
+    __vitePreload(() => import("./client.5b156202.js"), true ? [] : void 0)
+  ]);
+  renderer({
+    Component,
+    target: target$3,
+    props: props$3,
+    slots: { "default": "" },
+    isClientOnly: false
+  });
+});
+const props$2 = {};
+for (let propId of ["zaBf56", "2hM9Zg"]) {
+  const { name, value } = propsById[propId];
+  props$2[name] = value;
+}
+const target$2 = document.querySelector('slinkity-root[data-id="SnXFsU"]');
+Promise.race([
+  load()
+]).then(async function() {
+  const [{ default: Component }, { default: renderer }] = await Promise.all([
+    __vitePreload(() => import("./GatewaySelector.e0ec0ddf.js"), true ? ["assets/GatewaySelector.e0ec0ddf.js","assets/store.78950e7d.js"] : void 0),
+    __vitePreload(() => import("./client.5b156202.js"), true ? [] : void 0)
+  ]);
+  renderer({
+    Component,
+    target: target$2,
+    props: props$2,
+    slots: { "default": "" },
+    isClientOnly: false
+  });
+});
+const props$1 = {};
+for (let propId of ["c-R_7l"]) {
+  const { name, value } = propsById[propId];
+  props$1[name] = value;
+}
+const target$1 = document.querySelector('slinkity-root[data-id="3SbbNr"]');
+Promise.race([
+  load()
+]).then(async function() {
+  const [{ default: Component }, { default: renderer }] = await Promise.all([
+    __vitePreload(() => import("./IpfsLink.b7207e56.js"), true ? ["assets/IpfsLink.b7207e56.js","assets/store.78950e7d.js","assets/common.5d0d2fe0.js"] : void 0),
+    __vitePreload(() => import("./client.5b156202.js"), true ? [] : void 0)
+  ]);
+  renderer({
+    Component,
+    target: target$1,
+    props: props$1,
+    slots: { "default": "" },
+    isClientOnly: false
+  });
+});
+const props = {};
+for (let propId of ["AJD_HU"]) {
+  const { name, value } = propsById[propId];
+  props[name] = value;
+}
+const target = document.querySelector('slinkity-root[data-id="DOLS4C"]');
+Promise.race([
+  load()
+]).then(async function() {
+  const [{ default: Component }, { default: renderer }] = await Promise.all([
+    __vitePreload(() => import("./IpfsLink.b7207e56.js"), true ? ["assets/IpfsLink.b7207e56.js","assets/store.78950e7d.js","assets/common.5d0d2fe0.js"] : void 0),
+    __vitePreload(() => import("./client.5b156202.js"), true ? [] : void 0)
+  ]);
+  renderer({
+    Component,
+    target,
+    props,
+    slots: { "default": "" },
+    isClientOnly: false
+  });
+});
